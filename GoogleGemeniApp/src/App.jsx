acd43fd01bba93eef13364138ef45d5bcea4b3a2
Sanjai451 <sanjaikumarr451@gmail.com>
2024-03-07 20:57:53 +0530

    Google gemini with React and Google Gemini API

---
+++ GoogleGemeniApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import SideBar from './components/sidebar/SideBar'
+import Main from './components/main/Main'
+
+function App() {
+
+
+  return (
+    <>
+    <SideBar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ GoogleGemeniApp/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from 'react'
+import './SideBar.css'
+import {assets} from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const SideBar = () => {
+
+    const [extended , setExtended] = useState(false)
+    const {onSent,prevPrompt,setRecentPrompt,newChat}=useContext(Context)
+
+    const loadPrompt = async (prompt) =>{
+      setRecentPrompt(prompt)
+      await onSent(prompt)
+    }
+
+
+
+
+
+  return (
+    <div className='sidebar'>
+     <div className="top">
+       <img src={assets.menu_icon} className='menu' onClick={()=>setExtended(prev=>!prev)} />
+       <div onClick={()=>newChat()} className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            {extended?
+            <p>New Chat</p>            
+            :null}
+       </div>
+       {
+        extended ?
+        <div className="recent">
+        <p className="recent-title">Recent</p>
+        {prevPrompt.map((item,index)=>{
+          return (
+            <div className="recent-entry" onClick={()=>loadPrompt(item)}>
+               <img src={assets.message_icon} alt="" />
+            
+               <p>{item.slice(0,15)}...</p>           
+            
+            </div>
+          )
+        })}
+        </div>
+        :null
+       }
+      
+       
+     </div>
+     <div className="bottom">
+        <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="" />
+            {extended?<p>Help</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt="" />
+            {extended?<p>History</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt="" />
+            {extended?<p>Settings</p>:null}
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default SideBar
